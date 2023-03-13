9c5dd2c721c28bc80be1389a5d77ba64eb25df36
Lokochris <joseluismanco37@gmail.com>
2023-03-12 19:26:09 -0500

    Interactive homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,21 +1,13 @@
+import { Title } from "./HomePage/Title";
 import { ParticlesBack } from "./Particles";
 
 function Home() {
   return (
     <>
       <ParticlesBack />
-      <div className="flex flex-col justify-center items-center h-80 relative z-1">
-        <h1 className="font-bitter font-bold text-8xl text-main backdrop-blur-md py-10 px-20 border-2 border-main shadow-lg">
-          DOTLABS ( )
-        </h1>
-        {/* <p>
-            Entusiastas de la tecnología unidos para compartir conocimientos y
-            experiencias
-          </p>
-          <p>
-            Dotlabs() es una comunidad de desarrolladores. Somos un equipo que
-            comparte conocimientos, experiencias y grandes momentos.
-          </p> */}
+      <div className="flex flex-col justify-center items-center h-full ">
+        <Title />
+ 
       </div>
     </>
   );
