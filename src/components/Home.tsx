e85ac42817be52473096cef0f9b32e2cbfe1cc4a
Lokochris <joseluismanco37@gmail.com>
2023-03-13 23:33:54 -0500

    issues

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,16 +1,26 @@
-import { Title } from "./HomePage/Title";
+import styles from "@/styles/Home.module.css"
 import { ParticlesBack } from "./Particles";
 
-function Home() {
-  return (
-    <>
-      <ParticlesBack />
-      <div className="flex flex-col justify-center items-center h-full ">
-        <Title />
- 
-      </div>
-    </>
-  );
+
+function Home(){
+    return (
+      <>
+        <ParticlesBack />
+        <div className="flex flex-col justify-center items-center h-80 relative z-1">
+          <h1 className={`font-bitter font-bold text-5xl ${styles.title}`}>
+            DOTLABS ( )
+          </h1>
+          <p className="text-center font-bold">
+            Entusiastas de la tecnología unidos para compartir conocimientos y
+            experiencias
+          </p>
+          <p className="text-center">
+            Dotlabs() es una comunidad de desarrolladores. Somos un equipo que
+            comparte conocimientos, experiencias y grandes momentos.
+          </p>
+        </div>
+      </>
+    );
 }
 
-export { Home };
+export {Home}
