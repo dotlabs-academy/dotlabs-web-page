51f3ace481403f27e4b818053519e4fd19409ae1
Juan Fernando Cogollo Doria <cocodrilette@gmail.com>
2023-03-17 21:29:52 +0000

    RegistrationManager v1.0.0

--- _blockchain/src/RegistrationManager.sol
+++ _blockchain/src/RegistrationManager.sol
@@ -10,6 +10,13 @@ import "../lib/openzeppelin-contracts/contracts/security/ReentrancyGuard.sol";
 contract RegistrationManager is Initializable, Pausable, AccessControl, ReentrancyGuard {
     uint256 public registrationFee;
 
+    /// @notice When an user is `joined` it's mean that the registration fee was payed and locked
+    mapping(address => bool) isJoined;
+    /// @notice This is the admin confirmation about the user quota
+    mapping(address => bool) isConfirmed;
+    /// @dev This allow to reset the mappings above by iteration 
+    address[] users;
+
     event RegistrationFeeUpdated(address indexed admin, uint256 newFee, uint256 previosFee);
 
     modifier notContractSender() {
@@ -18,8 +25,8 @@ contract RegistrationManager is Initializable, Pausable, AccessControl, Reentran
     }
 
     modifier notZeroAddress(address _address) {
-        if (_address == address(0)) revert("ZeroAddress");
-        if (msg.sender == address(0)) revert("ZeroAddress");
+        if (_address == address(0)) revert("ZeroAddressFound");
+        if (msg.sender == address(0)) revert("ZeroAddressFound");
         _;
     }
 
@@ -46,12 +53,75 @@ contract RegistrationManager is Initializable, Pausable, AccessControl, Reentran
      * @dev The participant must not have already registered and must pay the registration fee.
      */
     function joinIn() external payable whenNotPaused notContractSender returns (bool) {
+        address sender = _getSenderSafe();
+        if (isConfirmed[sender]) revert("You are already confirmed");
+        if (isJoined[sender]) revert("You are alredy joined");
+        if (msg.value != registrationFee) revert("You sent invalid value.");
+        
+        isJoined[sender] = true;
+        users.push(sender);
+
         return true;
     }
 
-    function getRegisteredUsers() external view returns (address[] memory) {}
+    /**
+    * @param _userAddress The user address you want to confirm
+    * @notice Allow an admint o confirm a user quota.
+    * @return A boolean value indicating whether the operation was successful.
+    */
+    function confirmUserQuota(address _userAddress) external notZeroAddress(_userAddress) returns (bool) {
+        if (isConfirmed[_userAddress]) revert("AlreadyConfirmed");
+        if (!isJoined[_userAddress]) revert("NotJoined");
 
-    function reset() external whenPaused onlyRole(DEFAULT_ADMIN_ROLE) {}
+        isJoined[_userAddress] = false;
+        isConfirmed[_userAddress] = true;
+        
+        return true;
+    }
+
+    /**
+    * @param _usersAddresses The users addresses you want to confirm
+    * @notice Allow an admint o confirm a users quota.
+    * @return A boolean value indicating whether the operation was successful.
+    */
+    function confirmUserQuotaBatch(address[] memory _usersAddresses) external returns (bool) {
+        for (uint i = 0; i < _usersAddresses.length; i++) {
+            if (_usersAddresses[0] == address(0)) revert("ZeroAddressFound");
+            if (isConfirmed[_usersAddresses[i]]) revert("AlreadyConfirmed");
+            if (!isJoined[_usersAddresses[i]]) revert("NotJoined");
+
+            isJoined[_usersAddresses[i]] = false;
+            isConfirmed[_usersAddresses[i]] = true;
+        }            
+        return true;
+    }
+
+    /// @notice Set the user related state variables to their default value.
+    /// @dev Only users with `DEFAULT_ADMIN_ROLE` can executed it.
+    function reset() external onlyRole(DEFAULT_ADMIN_ROLE) {
+        for (uint i = 0; i < users.length; i++) {
+            address user = users[i];
+            isJoined[user] = false;
+            isConfirmed[user] = false;
+        }
+        delete users;
+    }
+
+    function refundFee(address _userAddress) external onlyRole(DEFAULT_ADMIN_ROLE) notZeroAddress(_userAddress) nonReentrant returns (bool success) {
+        (success, ) = payable(_userAddress).call{value: registrationFee}("");
+    }
+
+    function refundFeeBatch(address[] memory _usersAddresses) external onlyRole(DEFAULT_ADMIN_ROLE) nonReentrant returns (bool) {
+        for (uint i = 0; i < _usersAddresses.length; i++) {
+            (bool success, ) = payable(_usersAddresses[i]).call{value: registrationFee}("");
+            if (!success) revert("TransactionFailed");
+        } 
+        return true;
+    }
+
+    function getJoinedUsers() external view returns (address[] memory) {
+        return users;
+    } 
 
     /**
      * @notice Pauses the contract, preventing any further registration, confirmation, or unregistration actions.
